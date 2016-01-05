ddb602590934d3ef38f84e20c757a757e796ebe9
mcxaos <mcxaos@gmail.com>
2016-01-05 15:47:25 +0200

    refactoring

---
+++ protected/config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=web2',
+    'username' => 'root',
+    'password' => '',
+    'charset' => 'utf8',
+];
